5fe27082b60b2cbc607651ee3ad24c6ca1fa4aba
Morningstar-Don <donalynpantuhan16@gmail.com>
2024-12-01 18:52:44 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(page_title="My webpage", page_icon=":tada:", layout="wide")
+
+with st.container():
+    st.subheader("Hi, I'am Donalyn Pantuhan  :wave:")
+
+    st.title("MY BIOGRAPHY BLOG")
+   
+    st.write("INTRODUCTION")
+   
+    st.write("Hi I'am an aspiring computer engineer with a passion for problem-solving and innovation in technology") 
+    st.write("With a strong foundation in Computer engineering and a commitment to continuous learning")
+
+    st.title("About me")
+
+
+
+
+
